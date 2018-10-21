71673fedb8ecff7f3aebb75ad028baf8acb4f9ae
codefuldom <dominiquehallan@gmail.com>
2018-10-20 23:35:25 -0500

    routes complete

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -19,4 +19,33 @@ class EmployersController < ApplicationController
       end
     end
   end
+
+  def edit
+    @employer = Employer.find(params[:id])
+  end
+
+  def update
+    @employer = Employer.find(params[:id])
+
+    respond_to do |format|
+      if @employer.update(params.require(:employer).permit(:title, :subtitle, :body))
+        format.html { redirect_to employers_path, notice: 'Updated Successfully!' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def show
+    @employer = Employer.find(params[:id])
+  end
+
+  def destroy
+    @employer = Employer.find(params[:id])
+
+    @employer.destroy
+    respond_to do |format|
+      format.html { redirect_to employers_url, notice: ' Item was successfully destroyed.' }
+    end
+  end
 end
