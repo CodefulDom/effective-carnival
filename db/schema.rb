7b72dca1c25e3c3bd5eb44ed6f86aa107d2a91cd
codefuldom <dominiquehallan@gmail.com>
2018-10-20 20:01:22 -0500

    clear the air.

--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181019091334) do
-
+ActiveRecord::Schema.define(version: 20_181_019_091_334) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'blogs', force: :cascade do |t|
+    t.string 'title'
+    t.text 'body'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "employers", force: :cascade do |t|
-    t.string "title"
-    t.string "subtitle"
-    t.text "body"
-    t.text "main_image"
-    t.text "thumb_image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'employers', force: :cascade do |t|
+    t.string 'title'
+    t.string 'subtitle'
+    t.text 'body'
+    t.text 'main_image'
+    t.text 'thumb_image'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "skills", force: :cascade do |t|
-    t.string "title"
-    t.integer "percentage_utilized"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'skills', force: :cascade do |t|
+    t.string 'title'
+    t.integer 'percentage_utilized'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
