539557b42fecbd4295ba81fa259fc8511e24de64
codefuldom <dominiquehallan@gmail.com>
2018-10-20 19:02:20 -0500

    didnt pull..dumbass

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+		title: "My Blog Post #{blog}",
+		body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis aenean et tortor at risus viverra adipiscing at. Fames ac turpis egestas sed tempus. Aliquet risus feugiat in ante metus dictum. Nisl purus in mollis nunc sed id semper. Justo eget magna fermentum iaculis eu. Scelerisque eleifend donec pretium vulputate sapien. Nec feugiat nisl pretium fusce id. Placerat vestibulum lectus mauris ultrices eros in cursus. Arcu dictum varius duis at consectetur lorem donec massa. Quis imperdiet massa tincidunt nunc pulvinar sapien. Ac ut consequat semper viverra nam libero. Et malesuada fames ac turpis egestas integer eget aliquet nibh. Vestibulum sed arcu non odio euismod. Mattis enim ut tellus elementum sagittis vitae et leo. Imperdiet proin fermentum leo vel. Integer vitae justo eget magna fermentum iaculis eu non. Quis vel eros donec ac odio. Dui vivamus arcu felis bibendum ut tristique et egestas quis. Volutpat odio facilisis mauris sit amet massa.'
+  )
+end
+
+5.times do |skill|
+end
